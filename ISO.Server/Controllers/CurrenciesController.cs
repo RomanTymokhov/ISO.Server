9f580fe9f0ba746df4f8085978d79120c6b998ff
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-26 16:29:34 +0300

    Create Currencies Controller

---
+++ ISO.Server/Controllers/CurrenciesController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ISO.Server.Services;
+using ISO.Server.DTO;
+
+namespace ISO.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrenciesController : ControllerBase
+    {
+        private readonly IIsoService _isoService;
+
+        public CurrenciesController(IIsoService isoService)
+            => _isoService = isoService;
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Currency>> Get()
+        {
+            return Ok(_isoService.GetCurrensies());
+        }
+        
+        [HttpGet("{id}")]
+        public ActionResult<Currency> Get(string id)
+        {
+            var answ = CheckInput(id.ToUpper());
+            if (answ != null) return Ok(answ);
+            else return NotFound();
+        }
+
+        private Currency CheckInput(string input)
+        {
+            var cbc = _isoService.GetCurrencyByCode(input);
+            if (cbc == null) return _isoService.GetCurrencyBySimbol(input);
+            else return cbc;
+        }
+    }
+}
