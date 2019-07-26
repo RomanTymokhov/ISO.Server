00817b850c15fccb86a7e04d5b4ca64d351b3de0
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-26 15:47:15 +0300

    Summary IsoService
    Create & implement service

---
+++ ISO.Server/Services/IIsoService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ISO.Server.DTO;
+
+namespace ISO.Server.Services
+{
+    public interface IIsoService
+    {
+        ICollection<Currency> GetCurrensies();
+        Currency GetCurrencyByCode(string code);
+        Currency GetCurrencyBySimbol(string symbol);
+
+        ICollection<MerchantCategory> GetMerchantCategories();
+        MerchantCategory MerchantCategoryByCode(string code);
+        MerchantCategory GetMerchantCategoryById(int id);
+    }
+}
---
+++ ISO.Server/Services/Implements/IsoService.cs
@@ -0,0 +1,37 @@
+﻿using ISO.Server.DTO;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ISO.Server.Services.Implements
+{
+    public class IsoService : IIsoService
+    {
+        private readonly CurrencyTable currencyTable;
+        private readonly MccTable mccTable;
+
+        public IsoService(IOptions<DTO.ISO> isoOptions)
+        {
+            currencyTable = isoOptions.Value.ISO_4217.CcyTbl;
+            mccTable = isoOptions.Value.ISO_18245.MccTbl;
+        }
+
+        public Currency GetCurrencyByCode(string code)
+            => currencyTable.CcyNtry.FirstOrDefault(c => c.CcyNbr == code);
+
+        public Currency GetCurrencyBySimbol(string symbol)
+            => currencyTable.CcyNtry.FirstOrDefault(c => c.Ccy == symbol);
+
+        public ICollection<Currency> GetCurrensies()
+            => currencyTable.CcyNtry;
+
+        public ICollection<MerchantCategory> GetMerchantCategories()
+            => mccTable.MccNtry;
+
+        public MerchantCategory GetMerchantCategoryById(int id)
+            => mccTable.MccNtry.FirstOrDefault(m => m.Id == id);
+
+        public MerchantCategory MerchantCategoryByCode(string code)
+            => mccTable.MccNtry.FirstOrDefault(m => m.Mcc == code);
+    }
+}
